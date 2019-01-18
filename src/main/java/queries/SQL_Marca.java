a30b2da01d48f67948c91524c1e65432ca1cf203
ComanAdi <44721345+ComanAdi@users.noreply.github.com>
2019-01-18 22:26:16 +0200

    adaugat interfata erori si clasa model & marca

---
+++ src/main/java/queries/SQL_Marca.java
@@ -0,0 +1,20 @@
+package queries;
+
+import types.SQL_Operations_Interface;
+
+public class SQL_Marca implements SQL_Operations_Interface {
+    @Override
+    public void insert() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
