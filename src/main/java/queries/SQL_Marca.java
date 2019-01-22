d5ba73b272b4158a380bd3a776f8070df7ab8d54
ComanAdi <44721345+ComanAdi@users.noreply.github.com>
2019-01-22 20:10:58 +0200

    ziua z

--- src/main/java/queries/SQL_Marca.java
+++ src/main/java/queries/SQL_Marca.java
@@ -11,8 +11,7 @@ import java.sql.SQLException;
 public
 class SQL_Marca {
 
-    public
-    void insert(String marca) throws SQLException {
+    public void insert(String marca) throws SQLException {
         Connection connection = new DB_Connection().openDBConnection();
         PreparedStatement insertStatement = connection.prepareStatement("insert into `marca_auto` values (?, ?)");
 
@@ -38,8 +37,7 @@ class SQL_Marca {
     }
 
 
-    public
-    boolean checkDuplicateMarca(Connection connection, String marca) throws SQLException {
+    public boolean checkDuplicateMarca(Connection connection, String marca) throws SQLException {
         if (marca.length() == 0) {
             return false;
         }
@@ -66,69 +64,5 @@ class SQL_Marca {
     }
 
 
-    public
-    void update(String marca, String marca_noua) throws SQLException {
-        Connection connection = new DB_Connection().openDBConnection();
-        String updateMarca = "UPDATE marca_auto SET marca = ? , ";
 
-        PreparedStatement updateMarcaAuto = connection.prepareStatement(updateMarca);
-
-
-        updateMarcaAuto.setString(1, marca_noua);
-        updateMarcaAuto.executeUpdate();
-
-
-    }
-
-
-    //param intrare nume, nume nou
-    //verificare existenta si definita
-    //select dupa marca
-    //change SQL
-
-
-    public
-    void delete(String marca) throws SQLException {
-        Connection connection = new DB_Connection().openDBConnection();
-        PreparedStatement deleteStatement = connection.prepareStatement("DELETE FROM 'marca_auto' WHERE marca = ?");
-
-        if (marca.length() == 0) {
-            System.out.print(Errors.ERROR_DB_INVALID_MARCA);
-
-            new DB_Connection().closeDBConnection(connection);
-
-            return;
-        }
-
-        deleteStatement.setString(1, marca);
-        deleteStatement.executeUpdate();
-        new DB_Connection().closeDBConnection(connection);
-
-
-    }
-
-    public boolean checkExisting(Connection connection, String marca) throws SQLException {
-        if (marca.length() == 0) {
-
-            return false;
-        }
-
-        boolean existingMarca = false;
-
-        PreparedStatement searchStatement = connection.prepareStatement("select marca from `marca_auto` where marca = ?");
-
-        searchStatement.setString(1, marca);
-
-        ResultSet output = searchStatement.executeQuery();
-
-        if (!output.isBeforeFirst()) {
-            System.out.print(Errors.ERROR_DB_INVALID_MARCA);
-
-        } else {
-
-        }
-
-        return existingMarca;
-
-    }
 }
